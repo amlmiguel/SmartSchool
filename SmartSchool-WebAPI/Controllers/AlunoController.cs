e52a14b04cbabf5606b819e6128fa2c1e26a56e3
Miguel Leite <ava354@einstein.br>
2022-05-17 08:02:40 -0300

    CRUD Aluno

--- SmartSchool-WebAPI/Controllers/AlunoController.cs
+++ SmartSchool-WebAPI/Controllers/AlunoController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using SmartSchool_WebAPI.Data;
+using SmartSchool_WebAPI.Models;
 
 namespace SmartSchool_WebAPI.Controllers
 {
@@ -41,6 +42,90 @@ namespace SmartSchool_WebAPI.Controllers
       {
         return BadRequest($"Erro: {ex.Message}");
       }
+    }
+
+    [HttpGet("ByDisciplina/{disciplinaId}")]
+
+    public async Task<IActionResult> GetByDisciplinaId(int disciplinaId)
+    {
+      try
+      {
+        var result = await _repo.GetAlunosAsyncByDisciplinaId(disciplinaId, false);
+        return Ok(result);
+      }
+      catch (Exception ex)
+      {
+        return BadRequest($"Erro: {ex.Message}");
+      }
+    }
+
+    [HttpPost]
+
+    public async Task<IActionResult> post(Aluno model)
+    {
+      try
+      {
+        _repo.Add(model);
+
+        if(await _repo.SaveChangesAsync())
+        {
+          return Ok(model);
+        }
+      }
+      catch (Exception ex)
+      {
+        return BadRequest($"Erro: {ex.Message}");
+      }
+
+      return BadRequest();
+    }
+
+    [HttpPut("{alunoId}")]
+
+    public async Task<IActionResult> put(int alunoId, Aluno model)
+    {
+      try
+      {
+        var aluno = await _repo.GetAlunoAsyncById(alunoId, false)
+        if(aluno == null) return NotFound();
+
+        _repo.Update(model);
+
+        if(await _repo.SaveChangesAsync())
+        {
+          return Ok(model);
+        }
+      }
+      catch (Exception ex)
+      {
+        return BadRequest($"Erro: {ex.Message}");
+      }
+
+      return BadRequest();
+    }
+
+     [HttpDelete("{alunoId}")]
+
+    public async Task<IActionResult> delete(int alunoId, Aluno model)
+    {
+      try
+      {
+        var aluno = await _repo.GetAlunoAsyncById(alunoId, false);
+        if(aluno == null) return NotFound();
+
+        _repo.Delete(aluno);
+
+        if(await _repo.SaveChangesAsync())
+        {
+          return Ok("Deletado");
+        }
+      }
+      catch (Exception ex)
+      {
+        return BadRequest($"Erro: {ex.Message}");
+      }
+
+      return BadRequest();
     }
   }
 }
